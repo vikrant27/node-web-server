054c5c427f408aafdb7c91df4c33e2be40a6a336
Vikrant Mahajan <vikrantm93@gmail.com>
2017-11-12 01:34:39 +0530

    Add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about',(req,res) =>{
    });
 });
 
+app.get('/projects',(req,res) =>{
+   res.render('projects.hbs',{
+    pageTitle:'Projects'
+   });
+});
+
 app.listen(port,() =>{
 	console.log('server is online');
 });
